596a72ab3bbb798db117414e503a37186c399deb
RNaveenK20 <116283208+RNaveenK20@users.noreply.github.com>
2025-03-20 13:51:59 +0530

    Delete EmployeeDetailsAPI directory

--- EmployeeDetailsAPI/ClassModel/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EmployeeDetailsAPI.ClassModel
-{
-    public class LoginModel
-    {
-        public string userName { get; set; } = string.Empty;
-        public string password { get; set; } = string.Empty;
-    }
-}
